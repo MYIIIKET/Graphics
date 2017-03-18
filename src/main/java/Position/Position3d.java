f2ee96865a934d922711ba72b229b058995113e7
MYIIIKET <mylllket@yandex.ua>
2017-03-18 20:22:47 +0300

    to modify

--- src/main/java/Position/Position3d.java
+++ src/main/java/Position/Position3d.java
@@ -1,25 +1,34 @@
 package Position;
 
 public class Position3d extends Position2d {
-    private Coord2d zCoord;
+    private float zStart;
+    private float zEnd;
 
     public Position3d(float xStart, float yStart, float zStart, float xEnd, float yEnd, float zEnd) {
         super(xStart, yStart, xEnd, yEnd);
-        zCoord.setX(zStart);
-        zCoord.setY(zEnd);
+        setzStart(zStart);
+        setzEnd(zEnd);
     }
 
     public Position3d(Position2d position2d, float zStart, float zEnd) {
         super(position2d.getxStart(), position2d.getyStart(), position2d.getxEnd(), position2d.getyEnd());
-        zCoord.setX(zStart);
-        zCoord.setY(zEnd);
+        setzStart(zStart);
+        setzEnd(zEnd);
     }
 
-    public Coord2d getzCoord() {
-        return zCoord;
+    public float getzStart() {
+        return zStart;
     }
 
-    public void setzCoord(Coord2d zCoord) {
-        this.zCoord = zCoord;
+    public void setzStart(float zStart) {
+        this.zStart = zStart;
+    }
+
+    public float getzEnd() {
+        return zEnd;
+    }
+
+    public void setzEnd(float zEnd) {
+        this.zEnd = zEnd;
     }
 }
